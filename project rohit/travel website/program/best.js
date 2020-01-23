efc4cb5d4b747b2b3b858cd498113d290d7d5380
mrbrand05 <59999220+mrbrand05@users.noreply.github.com>
2020-01-23 17:09:19 +0530

    website travel
    using html5 css boostrap angular

---
+++ project rohit/travel website/program/best.js
@@ -0,0 +1,16 @@
+
+							<script type="text/javascript">
+								var a=angular.module("abc",[]);
+								a.controller("crt",function ($scope) {
+
+									$scope.db=[{id:"0",name:"niceshoes",price:"2000",img:"Hydrangeas.jpg",av:"available",sty:"panel-danger"},
+				{id:"1",name:"niceshoes",price:"2000",img:"Penguins.jpg",av:"available", sty:"panel-info"},
+									{id:"2",name:"reebock",price:"3000",img:"Hydrangeas.jpg",av:"not available,",sty:"panel-info"},
+									{id:"3",name:"reebookshoes",price:"4000",img:"Koala.jpg",av:"available",sty:"panel-primary"},
+									{id:"4",name:"nike shoes",price:"3000",img:"Tulips.jpg",av:"available",sty:"panel-success"},
+									{id:"5",name:"nike",price:"7000",img:"Hydrangeas.jpg",av:"available",sty:"panel-danger"},
+									{id:"6",name:"puma",price:"4000",img:"Chrysanthemum.jpg",av:"available"},
+									{id:"7",name:"haire",price:"1000",img:"Jellyfish.jpg",av:"available"},
+									{id:"8",name:"redchief",price:"3000",img:"Chrysanthemum.jpg",av:"not available"}]
+								})
+							</script>
